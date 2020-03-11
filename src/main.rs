587cc76518db600dce436c67462956c0874b9057
that1guy <thatguysemail007@gmail.com>
2020-03-10 22:33:33 -0400

    updates for ceaser cipher

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    //let alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
+    let mut encrypted = String::from("HVS EIWQY PFCKB TCL XIADG CJSF HVS ZONM RCU CT QOSGOF OBR MCIF IBWEIS GCZIHWCB WG CTTTUBTSAGBG");
+    unsafe {
+        let v_encrypted = encrypted.as_mut_vec();
+        let mut x = 1;
+        let mut i = 0;
+        while x != 26 {
+            while i != v_encrypted.len() {
+                //print!("FUCK...");
+                if v_encrypted[i] != ' ' as u8 {
+                    print!("{}", arithmetic(v_encrypted[i], x));
+                }
+                else{
+                    print!(" ");
+                }
+                i += 1;
+            }
+            i = 0;
+            x += 1;
+            println!();
+        }
+    }
+}
+
+fn arithmetic(letter: u8, delta: usize) -> u8 {
+
+    (letter + (delta as u8)) as u8
 }
